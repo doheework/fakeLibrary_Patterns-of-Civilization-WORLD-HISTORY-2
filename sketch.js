4ba959a7ea3ef047f2d7a2858a329368f7166719
Your Namedo <you@example.com>
2021-08-12 18:47:10 +0900

    12.08.1

---
+++ sketch.js
@@ -0,0 +1,272 @@
+$(document).ready(function(){
+    $(".칠판").mouseover(function(){
+        var image = $(".칠판i");
+    $(".칠판i").show();
+    $(".칠판i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".칠판").mouseout(function(){
+    $(".칠판i").stop().animate({top:"100%"});
+    
+    });
+    });
+
+$(document).ready(function(){
+    $(".책").mouseover(function(){
+        var image = $(".책i");
+    $(".책i").show();
+    $(".책i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".책").mouseout(function(){
+    $(".책i").stop().animate({top:"100%"});
+    
+    });
+    });
+
+$(document).ready(function(){
+    $(".책등").mouseover(function(){
+        var image = $(".책등i");
+    $(".책등i").show();
+    $(".책등i").stop().animate({top:window.innerHeight-image.innerHeight()});
+    });
+    $(".책등").mouseout(function(){
+    $(".책등i").stop().animate({top:"100%"});
+    
+    });
+    });
+
+$(document).ready(function(){
+    $(".world").mouseover(function(){
+        var image = $(".worldi");
+    $(".worldi").show();
+    $(".worldi").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".world").mouseout(function(){
+    $(".worldi").stop().animate({top:"100%"});
+    
+    });
+    });
+
+$(document).ready(function(){
+    $(".책등2").mouseover(function(){
+        var image = $(".책등2i");
+    $(".책등2i").show();
+    $(".책등2i").stop().animate({top:window.innerHeight-image.innerHeight()});
+    });
+    $(".책등2").mouseout(function(){
+    $(".책등2i").stop().animate({top:"100%"});
+    
+    });
+    });
+
+$(document).ready(function(){
+    $(".상단").mouseover(function(){
+        var image = $(".상단i");
+    $(".상단i").show();
+    $(".상단i").stop().animate({top:0});
+    });
+    $(".상단").mouseout(function(){
+    $(".상단i").stop().animate({top:"100%"});
+    
+    });
+    });
+
+$(document).ready(function(){
+    $(".docu").mouseover(function(){
+        var image = $(".docui");
+    $(".docui").show();
+    $(".docui").stop().animate({left:window.innerWidth-image.innerWidth()});
+    });
+    $(".docu").mouseout(function(){
+    $(".docui").stop().animate({left:"-100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".이전").mouseover(function(){
+        var image = $(".이전i");
+    $(".이전i").show();
+    $(".이전i").stop().animate({top:window.innerHeight-image.innerHeight()+5});
+    });
+    $(".이전").mouseout(function(){
+    $(".이전i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".두책").mouseover(function(){
+        var image = $(".두책i");
+    $(".두책i").show();
+    $(".두책i").stop().animate({top:window.innerHeight-image.innerHeight()+5});
+    });
+    $(".두책").mouseout(function(){
+    $(".두책i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".스톰").mouseover(function(){
+        var image = $(".스톰i");
+    $(".스톰i").show();
+    $(".스톰i").stop().animate({top:window.innerHeight-image.innerHeight()+5});
+    });
+    $(".스톰").mouseout(function(){
+    $(".스톰i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".패턴").mouseover(function(){
+        var image = $(".패턴i");
+    $(".패턴i").show();
+    $(".패턴i").stop().animate({top:window.innerHeight-image.innerHeight()+5});
+    });
+    $(".패턴").mouseout(function(){
+    $(".패턴i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".이미지").mouseover(function(){
+        var image = $(".이미지i");
+    $(".이미지i").show();
+    $(".이미지i").stop().animate({top:window.innerHeight-image.innerHeight()+5});
+    });
+    $(".이미지").mouseout(function(){
+    $(".이미지i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".그려").mouseover(function(){
+        var image = $(".그려i");
+    $(".그려i").show();
+    $(".그려i").stop().animate({top:window.innerHeight-image.innerHeight()+5});
+    });
+    $(".그려").mouseout(function(){
+    $(".그려i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".어린").mouseover(function(){
+        var image = $(".어린i");
+    $(".어린i").show();
+    $(".어린i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".어린").mouseout(function(){
+    $(".어린i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".피라미드").mouseover(function(){
+        var image = $(".피라미드i");
+    $(".피라미드i").show();
+    $(".피라미드i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".피라미드").mouseout(function(){
+    $(".피라미드i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".앤").mouseover(function(){
+        var image = $(".앤i");
+    $(".앤i").show();
+    $(".앤i").stop().animate({left:window.innerWidth-image.innerWidth()});
+    });
+    $(".앤").mouseout(function(){
+    $(".앤i").stop().animate({left:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".라").mouseover(function(){
+        var image = $(".라i");
+    $(".라i").show();
+    $(".라i").stop().animate({top:window.innerHeight-image.innerHeight()});
+    });
+    $(".라").mouseout(function(){
+    $(".라i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".새").mouseover(function(){
+        var image = $(".새i");
+    $(".새i").show();
+    $(".새i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".새").mouseout(function(){
+    $(".새i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".비석").mouseover(function(){
+        var image = $(".비석i");
+    $(".비석i").show();
+    $(".비석i").stop().animate({top:window.innerHeight-image.innerHeight()});
+    });
+    $(".비석").mouseout(function(){
+    $(".비석i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".te").mouseover(function(){
+        var image = $(".tei");
+    $(".tei").show();
+    $(".tet").show();
+    $(".tei").stop().animate({top:window.innerHeight-image.innerHeight()-30});
+    $(".tet").stop().animate({top:window.innerHeight-20});
+    });
+    $(".te").mouseout(function(){
+    $(".tei").stop().animate({top:"100%"});
+    $(".tet").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".돌연변이").mouseover(function(){
+        var image = $(".돌연변이i");
+    $(".돌연변이i").show();
+    $(".돌연변이i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".돌연변이").mouseout(function(){
+    $(".돌연변이i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".바스티유").mouseover(function(){
+        var image = $(".바스티유i");
+    $(".바스티유i").show();
+    $(".바스티유i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".바스티유").mouseout(function(){
+    $(".바스티유i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".권리선언").mouseover(function(){
+        var image = $(".권리선언i");
+    $(".권리선언i").show();
+    $(".권리선언i").stop().animate({left:"0%"});
+    });
+    $(".권리선언").mouseout(function(){
+    $(".권리선언i").stop().animate({left:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".장면").mouseover(function(){
+        var image = $(".장면i");
+    $(".장면i").show();
+    $(".장면i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".장면").mouseout(function(){
+    $(".장면i").stop().animate({top:"100%"});
+    });
+    });
